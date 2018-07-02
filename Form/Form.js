190b5caefc3455cf1a19f72d77928f88894d779a
ArthurHwang <ahwang55@gmail.com>
2018-07-02 16:42:08 -0700

    Added bootstrap card border to form #1

--- Form/Form.js
+++ Form/Form.js
@@ -2,18 +2,23 @@ import React from 'react'
 
 const Form = (props) => {
   return (
-    <form className="user-form" onSubmit={props.submit}>
-      <h1 className="title">Create a Flash Card</h1>
-      <div className="form-group">
-        <label>Question</label>
-        <input type="text" value={props.value.question} onChange={props.change} name="question" className="form-control" id="user-question-input" placeholder="Enter Question"/>
+    <div class="card-container card" >
+      <div class="card-body">
+        <form className="user-form" onSubmit={props.submit}>
+          <h1 className="title">Create a Flash Card</h1>
+          <div className="form-group">
+            <label>Question</label>
+            <input type="text" value={props.value.question} onChange={props.change} name="question" className="form-control" id="user-question-input" placeholder="Enter Question"/>
+          </div>
+          <div className="form-group">
+            <label>Answer</label>
+            <input type="text" value={props.value.answer} onChange={props.change} name="answer" className="form-control" id="user-answer-input" placeholder="Enter Answer"/>
+          </div>
+          <button type="submit" className="save-btn btn btn-primary">Save</button>
+        </form>
       </div>
-      <div className="form-group">
-        <label>Answer</label>
-        <input type="text" value={props.value.answer} onChange={props.change} name="answer" className="form-control" id="user-answer-input" placeholder="Enter Answer"/>
-      </div>
-      <button type="submit" className="save-btn btn btn-primary">Save</button>
-    </form>
+    </div>
+
   )
 }
 
