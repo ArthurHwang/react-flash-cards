0b06c99ddda778a42887a8c9f346b648d5685dcf
ArthurHwang <ahwang55@gmail.com>
2018-07-02 14:19:52 -0700

    Define a form component that renders a form with two inputs and a save button #1

---
+++ Form/Form.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Form = (props) => {
+
+  return (
+    <form>
+      <div className="form-group">
+        <label for="user-question">Question</label>
+        <input type="text" className="form-control" id="user-question-input" placeholder="Enter Question"/>
+      </div>
+      <div className="form-group">
+        <label for="user-answer">Answer</label>
+        <input type="text" className="form-control" id="user-answer-input" placeholder="Enter Question"/>
+      </div>
+      <button type="submit" className="btn btn-primary" id="save-button">Save</button>
+    </form>
+  )
+}
+
+export default Form
