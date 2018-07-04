54b28e48ab5cd001c3db19a7bf8b8b7a34ba79a6
ArthurHwang <ahwang55@gmail.com>
2018-07-04 15:23:42 -0700

    Style the edit button #4

--- FlashCard/FlashCard.js
+++ FlashCard/FlashCard.js
@@ -2,17 +2,14 @@ import React from 'react'
 
 const FlashCard = ({question, answer}) => {
   return (
-    <div className="card">
+    <div className="flashcard card">
       <div className="card-header bg-warning">
       </div>
       <div className="card-body">
         <h5 className="card-title">{question}</h5>
         <p className="card-text">{answer}</p>
       </div>
-      <button><i class="edit fas fa-edit"></i>Edit</button>
-
-
-
+      <button className="text-primary edit">Edit<i class="fas fa-edit"></i></button>
     </div>
   )
 }
