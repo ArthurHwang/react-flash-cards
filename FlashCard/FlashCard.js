e182da7d7626ac4b23bd30a9dc687198fb750e44
ArthurHwang <ahwang55@gmail.com>
2018-07-03 20:26:48 -0700

    Removed edit and delete buttons, removed global styling for certain elements #2

--- FlashCard/FlashCard.js
+++ FlashCard/FlashCard.js
@@ -11,8 +11,6 @@ const FlashCard = (props) => {
       <div className="card-body">
         <h5 className="card-title">{props.question}</h5>
         <p className="card-text">{props.answer}</p>
-        <a className="edit-btn btn btn-primary">edit</a>
-        <a className="delete-btn btn btn btn-primary">delete</a>
       </div>
     </div>
   )
--- Form/Empty.js
+++ Form/Empty.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Empty = (props) => {
   return (
-    <div className="empty">
-      <h1>You have no flash cards</h1>
-      <button href="#new" id="make-one-button" onClick={props.click}>Make one</button>
+    <div className="text-center">
+      <h1 className="text-center">You have no flash cards</h1>
+      <button  className="btn-lg btn btn-success" href="#new" id="make-one-button" onClick={props.click}>Make one</button>
     </div>
   )
 }
