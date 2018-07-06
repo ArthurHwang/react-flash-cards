0b8bacc1547733e8602583f19f2454c1aa5e1a6b
ArthurHwang <ahwang55@gmail.com>
2018-07-05 19:51:42 -0700

    added eslint config for react-babel, applied lintetext to top #5

--- FlashCard/FlashCard.js
+++ FlashCard/FlashCard.js
@@ -1,17 +1,23 @@
 import React from 'react'
 
-const FlashCard = ({question, answer, edit, id}) => {
-  return (
-    <div data-id={id} className="flashcard card">
-      <div className="card-header bg-warning">
-      </div>
-      <div className="card-body">
-        <h5 className="card-title">{question}</h5>
-        <p className="card-text">{answer}</p>
-        <button onClick={edit} className="text-primary edit">Edit <i className="fas fa-edit"></i></button>
-      </div>
+const FlashCard = ({
+  question, answer, edit, id,
+}) => (
+  <div data-id={id} className="flashcard card">
+    <div className="card-header bg-warning" />
+    <div className="card-body">
+      <h5 className="card-title">
+        {question}
+      </h5>
+      <p className="card-text">
+        {answer}
+      </p>
+      <button onClick={edit} className="text-primary edit">
+        Edit
+        <i className="fas fa-edit" />
+      </button>
     </div>
-  )
-}
+  </div>
+)
 
 export default FlashCard
--- FlashCard/FlashCards.js
+++ FlashCard/FlashCards.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import FlashCard from './FlashCard'
-import Empty from '../Form/Empty.js'
+import Empty from '../Form/Empty'
 
-const FlashCards = ({edit, click, data}) => {
+const FlashCards = ({ edit, click, data }) => {
   const cardData = [...data]
-  let cards = <Empty click={click}/>
+  let cards = <Empty click={click} />
   if (data.length) {
-    cards = cardData.map((elem, index) => {
-      return <FlashCard id={index} edit={edit} key={index} question={elem.question} answer={elem.answer}/>
-    })
+    cards = cardData.map((elem, index) => (
+      <FlashCard
+        id={index}
+        edit={edit}
+        key={index}
+        question={elem.question}
+        answer={elem.answer}
+      />
+    ))
   }
-  return cards
+
+  return (
+    <div className="card-container">
+      {cards}
+    </div>
+  )
 }
 
 export default FlashCards
